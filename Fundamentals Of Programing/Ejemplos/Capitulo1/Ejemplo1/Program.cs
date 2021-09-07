1e68ce20882240c3d980c42c058ca6119ac210a7
carlos123che <38817231+carlos123che@users.noreply.github.com>
2021-09-07 16:02:35 -0600

    First Commit

---
+++ Fundamentals Of Programing/Ejemplos/Capitulo1/Ejemplo1/Program.cs
@@ -0,0 +1,33 @@
+﻿//Program 1
+//Contents: source code for example 1
+//Date last modified: 07/09/2021
+//Version 1.0
+//Author: Carlos Che
+//Name: Program1
+
+using System;
+
+namespace Program1
+{
+
+    class Program
+    {
+        const int VERSION = 1; //global
+        static void Main(string[] args)
+        {
+            //DECLARE AND INITIALIZE STRINGS
+            String name = "Carlos Ché";
+            String course = "CS1400 Section 001";
+            String version = "Version " + VERSION;
+            String project = "Program One";
+            String date = "September 07 of 2021";
+
+            //DISPLAY THE STRINGS
+            Console.WriteLine(name);
+            Console.WriteLine(course);
+            Console.WriteLine(version);
+            Console.WriteLine(project);
+            Console.WriteLine(date);
+        }
+    }
+}
